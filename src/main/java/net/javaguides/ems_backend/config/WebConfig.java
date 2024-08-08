96950cc19751bc72bb02702ac28377ca0f385802
MinhDinh <MinhDDHE176148@fpt.edu.vn>
2024-08-08 15:03:16 +0700

    fixed s

---
+++ src/main/java/net/javaguides/ems_backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package net.javaguides.ems_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply CORS settings to all paths
+            .allowedOrigins("http://localhost:3000", "https://minh-dinh2003.github.io/ems-frontend/") // Add allowed origins
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add allowed methods
+            .allowedHeaders("*"); // Allow all headers
+    }
+}
