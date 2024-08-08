57556012b00eb1c629376bb86649ff121f099190
MinhDinh <MinhDDHE176148@fpt.edu.vn>
2024-08-08 17:11:27 +0700

    commit

--- src/main/java/net/javaguides/ems_backend/config/WebConfig.java
+++ src/main/java/net/javaguides/ems_backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS settings to all paths
-            .allowedOrigins("*") // Add allowed origins
+            .allowedOrigins(Arrays.asList("https://beneficial-comfort-production.up.railway.app/")) // Add allowed origins
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add allowed methods
             .allowedHeaders("*"); // Allow all headers
     }
