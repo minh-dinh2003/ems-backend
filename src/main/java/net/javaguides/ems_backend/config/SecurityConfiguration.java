d060cf62bae63e7d16c585ba1758be51c3da43ba
MinhDinh <MinhDDHE176148@fpt.edu.vn>
2024-08-08 15:16:56 +0700

    add cor2

--- src/main/java/net/javaguides/ems_backend/config/SecurityConfiguration.java
+++ src/main/java/net/javaguides/ems_backend/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package net.javaguides.ems_backend.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,15 +28,20 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .cors() // Enable CORS
+            .csrf()
+            .disable()
+            .cors()
             .and()
             .authorizeHttpRequests()
-            .requestMatchers("/api/v1/auth/**").permitAll()
-            .anyRequest().authenticated()
+            .requestMatchers("/api/v1/auth/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
             .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
+            .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         
         return http.build();
